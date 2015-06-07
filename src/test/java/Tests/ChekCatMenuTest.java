9b1e2ff7cecf2df13995cc8ab62e0e413af0a202
Vlad <vsologub90@gmail.com>
2015-06-08 00:54:44 +0300

    refactoring

--- src/test/java/Tests/ChekCatMenuTest.java
+++ src/test/java/Tests/ChekCatMenuTest.java
@@ -1,18 +1,13 @@
 package Tests;
 
 import Pages.Header;
-import Pages.User;
 import helper.Helper;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.After;
-
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -21,13 +16,11 @@ public class ChekCatMenuTest {
     private WebDriver driver;
     private static final String url="http://allo.ua/";
     private static Header header;
-    private static User user;
     
     @Before
     public void SetUp() {
         driver=Helper.Setup(url);
         header=new Header(driver);
-        user=new User(driver);
     }
     
     @After
@@ -50,16 +43,9 @@ public class ChekCatMenuTest {
             "Дом, сад, ремонт",
             "Акции и скидки"
         };
-        
-        //header.createDataCats(main_menu);
                 
         for(String s: main_menu)
             item_menu_names.addAll(header.getAllItemsFromMenu(s));
         assertThat(item_menu_names, is(Helper.getListFromFile("categories")));
-        //assertArrayEquals(Arrays.asList(item_menu_names), Arrays.asList(Helper.getListFromFile("categories")));
-        //assertTrue(header.compareCats(item_menu_names, "categories"));
-    }
-
-    private void assertArrayEquals(List<List<String>> item_menu_names, List<List<String>> ccategories) {
     }
 }
